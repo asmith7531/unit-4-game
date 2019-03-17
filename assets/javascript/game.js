83ab1e1a24e8069bad5e31dbc5393cb0a16fe6bf
DocHacksaw <austinsmith7531@gmail.com>
2019-03-16 20:33:32 -0400

    ;alkdjsf;lk

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,42 @@
 $(document).ready(function()  {
-  var target = Math.floor(Math.random()*101+19)
-  var crystal1val = Math.floor(Math.random()*12+1)
-  var crystal2val = Math.floor(Math.random()*12+1)
-  var crystal3val = Math.floor(Math.random()*12+1)
-  var crystal4val = Math.floor(Math.random()*12+1)
-  var userScore = $("#userScore").val()
 
-  function gamestart(){
+  //decalaring the global variables
+  var target = Math.floor(Math.random()*101+19);
+  var crystal1val = Math.floor(Math.random()*12+1);
+  var crystal2val = Math.floor(Math.random()*12+1);
+  var crystal3val = Math.floor(Math.random()*12+1);
+  var crystal4val = Math.floor(Math.random()*12+1);
+  var userScore = $(document).val($("userScore"));
+  console.log(crystal1val)
 
-    var wins = parseInt($("#wins").val(),10)
-    var losses = parseInt($("#losses").val(),10)
+  //game start function 
+  
+    //variables to keep score
+    var wins = parseInt($("#wins").val(),10);
+    var losses = parseInt($("#losses").val(),10);
+    
+    //writes the target number to the span so the user can see it in the HTML
+    $(".targetNumber").text(target);
 
-    $(".targetNumber").text(target)
-
+    //function that adds the random crystal number to the users score
     function input(crystal){
       $("userScore").text(userScore + crystal)
     }
 
+    //calls our input function when we click on the crystal buttons
     $("#crystal1").click(input(crystal1val))
-    $("#crystal1").click(input(crystal2val))
-    $("#crystal1").click(input(crystal3val))
-    $("#crystal1").click(input(crystal4val))
+    $("#crystal2").click(input(crystal2val))
+    $("#crystal3").click(input(crystal3val))
+    $("#crystal4").click(input(crystal4val))
 
+    //logic that determines when the round is over and if the user wins or loses
     if (userScore === target) {
-      alert("You Win!")
-      wins++
+      alert("You Win!");
+      wins++;
     }
     else if (userScore > target)  {
-      alert("You Lose!")
-      losses++
-      gamestart()
+      alert("You Lose!");
+      losses++;
     }
-    gamestart()
-  }
   }
 )
