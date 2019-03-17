2405a261f330dd050bc62fcdf755462ab0c0f8a7
DocHacksaw <austinsmith7531@gmail.com>
2019-03-16 21:00:04 -0400

    I am so frustrated right now... I tried all day to make this work using loops to create my crystal elements and now the whole thing is broken and I dont have time to fix it. I am going to try and start over.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,62 @@
 $(document).ready(function()  {
+  var userScore = 0;
   
+  var wins = 0;
+  var losses = 0;
+  var target;
+ 
+  target = Math.floor(Math.random()*101+19);
+
+  function start()  {
+    $(".targetNumber").html("The computer chose the number " + target + ".")
+
+    var images = ["https://www.google.com/imgres?imgurl=https%3A%2F%2Fvignette.wikia.nocookie.net%2Fmgefanon%2Fimages%2F6%2F69%2FKinetic_Crystal.jpg%2Frevision%2Flatest%3Fcb%3D20150409231719&imgrefurl=https%3A%2F%2Fmgefanon.fandom.com%2Fwiki%2FKinetic_Crystal&docid=_542IdKX_LnmkM&tbnid=8HkvRCLa2WsljM%3A&vet=10ahUKEwiY66Hx7oXhAhVJnuAKHXmbBGsQMwh6KA8wDw..i&w=314&h=524&safe=off&bih=888&biw=1920&q=crystal&ved=0ahUKEwiY66Hx7oXhAhVJnuAKHXmbBGsQMwh6KA8wDw&iact=mrc&uact=8", "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjDnr-A74XhAhXMJt8KHUC6DcEQjRx6BAgBEAU&url=https%3A%2F%2Fwww.amazon.com%2Frockcloud-Titanium-Decoration-Gemstone-Specimen%2Fdp%2FB01G3EECXM&psig=AOvVaw1ajZS5kZzMwusNidd3BHGI&ust=1552798382709474", "https://www.google.com/imgres?imgurl=https%3A%2F%2Fracichemedcentral.com.au%2Fimages%2Fcrystal-growing.png&imgrefurl=https%3A%2F%2Fracichemedcentral.com.au%2Fcrystal-growing&docid=ACKnT66OXcBVIM&tbnid=2mEpKM8-Et0vqM%3A&vet=12ahUKEwjShJeX74XhAhVMmuAKHTZkDtI4yAEQMygIMAh6BAgBEAk..i&w=301&h=347&safe=off&bih=888&biw=1920&q=crystal&ved=2ahUKEwjShJeX74XhAhVMmuAKHTZkDtI4yAEQMygIMAh6BAgBEAk&iact=mrc&uact=8", "https://www.google.com/imgres?imgurl=https%3A%2F%2Fvignette.wikia.nocookie.net%2Fstarwars%2Fimages%2F5%2F5f%2FLightsaberCrystal-SWE.png%2Frevision%2Flatest%3Fcb%3D20160911062335&imgrefurl=https%3A%2F%2Fstarwars.fandom.com%2Fwiki%2FCrystal&docid=cs4ku-DfNdmCcM&tbnid=tLHpLVZBaEBinM%3A&vet=12ahUKEwiJwveL74XhAhVtQt8KHVBrB1k4ZBAzKFgwWHoECAEQWg..i&w=340&h=615&safe=off&bih=888&biw=1920&q=crystal&ved=2ahUKEwiJwveL74XhAhVtQt8KHVBrB1k4ZBAzKFgwWHoECAEQWg&iact=mrc&uact=8"]
+      
+      for(var i=1; i<=4; i++){
+         crystalVal = Math.floor(Math.random()*11+1);
+        var crystal = $("<div>")
+        crystal.attr({
+          "class":'crystal' + i,
+          "value":crystalVal
+        }
+        )
+        crystal.value = crystalVal;
+        console.log(crystal.value)
+        crystal.css({
+          "background-image": url=images[i]
+        })
+
+        $(".crystal").append(crystal)
+       
+        // $(".crystal").click( function () {
+        //   console.log(this.value)
+        // })
+      }
+     
+      
+      $(document).on('click', ".crystal", function () {
+        console.log(this.value)
+      var number = $(this).value;
+      // console.log(number)
+    
+      userScore += number
+      $("#userScore") .text (userScore);
+      console.log(userScore)
+
+    // })
   
-    var target = Math.floor(Math.random()*101+19)
-    var crystal1val = Math.floor(Math.random()*12+1)
-    var crystal2val = Math.floor(Math.random()*12+1)
-    var crystal3val = Math.floor(Math.random()*12+1)
-    var crystal4val = Math.floor(Math.random()*12+1)
-    var userScore = $("userScore");
-    console.log(userScore)
-    $(".targetNumber").innerHTML = target
-    $("#crystal1").click(function(){
-      $("#userScore").innerHTML = crystal1val + parseInt(userScore)
-    })
-  
+      })
+    if (userScore===target){
+      confirm("You Win!");
+      wins++;
+      start();
+    }
+    else if (userScore>target){
+      confirm("You Lose!");
+      losses++;
+      start();
+    }
   }
-  
-)
+  start()  
+
+})
